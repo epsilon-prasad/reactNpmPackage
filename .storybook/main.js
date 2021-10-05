24a87a74709b9fe799b70a5f4d938857ba1cbb42
epsilon-prasad <prasad.u@epsilon.com>
2021-10-05 18:30:17 +0530

    Adding the typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ["../packages/**/*.stories.js", "../packages/**/*.stories.jsx"],
+  stories: ["../packages/**/*.stories.js", "../packages/**/*.stories.jsx", "../packages/**/*.stories.tsx"],
   addons: ["@storybook/addon-docs"]
 };
