532b5c3bf7a2199d8603fa7d7a611e8e784f280c
epsilon-prasad <prasad.u@epsilon.com>
2021-10-07 16:32:33 +0530

    adding the subText

--- packages/heading/src/index.jsx
+++ packages/heading/src/index.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Heading = ({label, subText}) =>{
     return(
         <>
-        <h1>{label}</h1>
-        <p>{subText}</p>
+            <h1>{label}</h1>
+            <p style={{color: "red"}}>{subText}</p>
         </>
     )
 }
