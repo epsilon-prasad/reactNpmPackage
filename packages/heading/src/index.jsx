ea98c13f744271bfb706c8cbe0163b2223ec25fa
epsilon-prasad <prasad.u@epsilon.com>
2021-10-07 16:50:40 +0530

    Build check

--- packages/heading/src/index.jsx
+++ packages/heading/src/index.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Heading = ({label, subText}) =>{
     return(
         <>
-            <h1>{label}</h1>
-            <p>{subText}</p>
+            <h1 style={{color: "blue"}}>{label}</h1>
+            <p style={{color: "red"}}>{subText}</p>
         </>
     )
 }
