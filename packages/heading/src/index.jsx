2e11caa5686a603c5f56e52c69658c328e00b76b
epsilon-prasad <prasad.u@epsilon.com>
2021-10-07 16:42:00 +0530

    Updating the style for heading

--- packages/heading/src/index.jsx
+++ packages/heading/src/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Heading = ({label, subText}) =>{
     return(
         <>
-            <h1>{label}</h1>
+            <h1 style={{color: "blue"}}>{label}</h1>
             <p style={{color: "red"}}>{subText}</p>
         </>
     )
